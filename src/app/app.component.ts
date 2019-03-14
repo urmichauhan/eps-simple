3f4d63a6582eaba955fd9b5b71379a193ad15ccd
urmichauhan <chauhanurmi2@gmail.com>
2019-03-14 13:40:22 +0530

    eps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { shallowEqual } from "@angular/router/src/utils/collection";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'eps';
+  public s = false;
+
+  title = "eps";
+  show() {
+    this.s = true;
+    console.log("show menu");
+  }
 }
